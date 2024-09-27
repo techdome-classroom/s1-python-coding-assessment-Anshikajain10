f1a05f13e587101ad2ee084399b78fa21b8995bd
Anshikajain10 <anshikarawat1011@gmail.com>
2024-09-27 13:07:41 +0530

    test1.py

--- test1.py
+++ test1.py
@@ -6,9 +6,9 @@ class Solution:
         def dfs(i, j):
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == "W":
                 return
-            # Mark the current land as visited by turning it into water
+            
             grid[i][j] = "W"
-            # Explore the neighboring lands in all 4 directions
+            
             dfs(i+1, j)
             dfs(i-1, j)
             dfs(i, j+1)
@@ -18,8 +18,8 @@ class Solution:
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "L":  # Start a DFS for every unvisited land
+                if grid[i][j] == "L":  
                     total_islands += 1
-                    dfs(i, j)  # Perform DFS to mark all connected land as visited
+                    dfs(i, j)  
         
         return total_islands
