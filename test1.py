2a4ec7c356583e936eb0ba7539b766c30c6186c5
Anshikajain10 <anshikarawat1011@gmail.com>
2024-09-27 12:44:28 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,21 +1,25 @@
-import unittest
-from program1 import Solution
-
-class TestSolution(unittest.TestCase):
-    def setUp(self):
-        self.solution = Solution()
-
-    def test_case1(self):
-        result = self.solution.getTotalIsles([["L","L","L","L","W"],["L","L","W","L","W"],["L","L","W","W","W"],["W","W","W","W","W"]])
-        self.assertEqual(result, 1)
-
-    def test_case2(self):
-        result = self.solution.getTotalIsles([["L","L","W","W","W"],["L","L","W","W","W"],["W","W","L","W","W"],["W","W","W","L","L"]])
-        self.assertEqual(result, 3)
-
-    def test_case3(self):
-        result = self.solution.getTotalIsles([["W", "W", "W", "W"], ["W", "L", "L", "W"], ["W", "L", "L", "W"], ["W", "W", "W", "W"]])
-        self.assertEqual(result, 1)
-
-if __name__ == '__main__':
-    unittest.main(argv=['first-arg-is-ignored'], exit=False)
+class Solution:
+    def getTotalIsles(self, grid):
+        if not grid or not grid[0]:
+            return 0
+        
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == "W":
+                return
+            # Mark the current land as visited by turning it into water
+            grid[i][j] = "W"
+            # Explore the neighboring lands in all 4 directions
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+        
+        total_islands = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "L":  # Start a DFS for every unvisited land
+                    total_islands += 1
+                    dfs(i, j)  # Perform DFS to mark all connected land as visited
+        
+        return total_islands
